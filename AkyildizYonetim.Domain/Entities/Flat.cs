224933af5aa3ae9805bed236bdd34ca26a0e326d
Ensar <130212899+EnsarOZD@users.noreply.github.com>
2025-07-01 22:47:18 +0300

    Update Flat.cs

--- AkyildizYonetim.Domain/Entities/Flat.cs
+++ AkyildizYonetim.Domain/Entities/Flat.cs
@@ -6,6 +6,10 @@ public class Flat : BaseEntity
     public int Floor { get; set; } // Kat
     public Guid OwnerId { get; set; }
     public Guid? TenantId { get; set; }
+    public string ApartmentNumber { get; set; } = string.Empty;
+    public int RoomCount { get; set; }
+    public bool IsActive { get; set; } = true;
+    
 
     // Navigation properties
     public virtual Owner Owner { get; set; } = null!;
